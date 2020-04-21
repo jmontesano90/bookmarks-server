0a4c56522735cd953a2152a50e893943fa9b232d
Joseph Montesano <jmmmontesano@gmail.com>
2020-04-21 17:33:39 -0400

    modifed the postgrator config

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  //DB_URL: process.env.DB_URL || "postgresql://postgres@localhost/bookmarks",
-  DB_URL:
-    process.env.NODE_ENV === "test"
-      ? process.env.TEST_DB_URL
-      : process.env.DB_URL,
+  DB_URL: process.env.DB_URL || "postgresql://postgres@localhost/bookmarks",
+  // DB_URL:
+  //   process.env.NODE_ENV === "test"
+  //     ? process.env.TEST_DB_URL
+  //     : process.env.DB_URL,
 };
